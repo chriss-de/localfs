27405e129bbdcb2013e0ce7409cf2966642b85e4
Christoph Hartmann <christoph.hartmann@aeb.com>
2025-02-27 14:24:41 +0100

    Added tryFiles feature

--- main.go
+++ main.go
@@ -9,28 +9,67 @@ import (
 
 type LocalFS struct {
 	rootPath string
+	tryFiles []string
 }
 
-func NewLocalFS(rootPath string) (_ *LocalFS, err error) {
+func NewLocalFS(rootPath string, opts ...func(lfs *LocalFS)) (lfs *LocalFS, err error) {
 	var rootPathAbs string
 	if rootPathAbs, err = filepath.Abs(rootPath); err != nil {
 		return nil, err
 	}
-	return &LocalFS{rootPath: rootPathAbs}, nil
+
+	lfs = &LocalFS{rootPath: rootPathAbs}
+
+	for _, opt := range opts {
+		opt(lfs)
+	}
+
+	return lfs, nil
 }
 
-func (lfs *LocalFS) Open(name string) (_ fs.File, err error) {
+func WithTryFile(files string) func(lfs *LocalFS) {
+	return func(lfs *LocalFS) {
+		lfs.tryFiles = append(lfs.tryFiles, files)
+	}
+}
+
+func WithTryFiles(files ...string) func(lfs *LocalFS) {
+	return func(lfs *LocalFS) {
+		lfs.tryFiles = append(lfs.tryFiles, files...)
+	}
+}
+
+func (lfs *LocalFS) Open(name string) (fd fs.File, err error) {
 	name = filepath.Join(lfs.rootPath, name)
 	if name, err = filepath.Abs(name); err != nil {
 		return nil, err
 	}
-	if strings.HasPrefix(name, lfs.rootPath) {
-		return os.Open(name)
+	if !strings.HasPrefix(name, lfs.rootPath) {
+		return nil, os.ErrNotExist
 	}
-	return nil, os.ErrNotExist
+
+	return lfs.tryOpen(name)
 }
 
-//func (lfs *LocalFS) ReadFile(name string) ([]byte, error) {
-//	slog.Info("localFS/ReadFile", "name", name)
-//	return nil, nil
-//}
+func (lfs *LocalFS) tryOpen(name string) (fd fs.File, err error) {
+	if fd, err = os.Open(name); err != nil {
+		if !os.IsNotExist(err) {
+			return nil, err
+		}
+		for _, tryFile := range lfs.tryFiles {
+			if fd, err = os.Open(tryFile); err != nil {
+				if !os.IsNotExist(err) {
+					return nil, err
+				}
+			} else {
+				break
+			}
+		}
+
+		if fd == nil {
+			return nil, os.ErrNotExist
+		}
+	}
+
+	return fd, nil
+}
